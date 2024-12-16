b18676314aea6d07f4fe5361bd6d9e03759a9567
Stefan <git@stefn.co.uk>
2024-12-16 18:16:47 +0000

    Nixier

---
+++ default.nix
@@ -0,0 +1,51 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # Input source files
+  src = ./.;
+  nodeDeps = import ./node-deps.nix { inherit pkgs; };
+  inherit (nodeDeps) packageJSON nodeModules;
+
+in
+pkgs.stdenv.mkDerivation {
+  name = "blog-chobble-com";
+
+  src = builtins.filterSource
+    (path: type: !(builtins.elem (baseNameOf path) [
+      "_site"
+      "node_modules"
+      ".git"
+    ]))
+    src;
+
+  nativeBuildInputs = with pkgs; [
+    cacert
+    minify
+    lightningcss
+    sass
+    yarn
+  ];
+
+  configurePhase = ''
+    export HOME=$TMPDIR
+    mkdir -p _site/style
+
+    cp -r ${nodeModules}/node_modules .
+    chmod -R +w node_modules
+    cp ${packageJSON} package.json
+  '';
+
+  buildPhase = ''
+    echo 'Building site'
+    yarn --offline eleventy
+
+    echo 'Minifying HTML'
+    minify --all --recursive --output . _site
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r _site/* $out/
+    rm -rf node_modules _site package.json
+  '';
+}
---
+++ shell.nix
@@ -0,0 +1,90 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+let
+  nodeDeps = import ./node-deps.nix { inherit pkgs; };
+  inherit (nodeDeps) packageJSON nodeModules;
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    sass
+    yarn
+  ];
+
+  shellHook = ''
+    rm -rf node_modules
+    rm -rf package.json
+
+    ln -sf ${packageJSON} package.json
+    ln -sf ${nodeModules}/node_modules .
+
+    serve() {
+      yarn eleventy --serve &
+      ELEVENTY_PID=$!
+
+      sass --watch src/_scss:_site/css --style compressed
+      SASS_PID=$!
+
+      cleanup_serve() {
+        echo "Cleaning up serve processes..."
+        kill $ELEVENTY_PID 2>/dev/null
+        wait $ELEVENTY_PID 2>/dev/null
+        kill $SASS_PID 2>/dev/null
+        wait $SASS_PID 2>/dev/null
+        cleanup
+      }
+
+      trap cleanup_serve EXIT INT TERM
+
+      wait -n
+
+      cleanup_serve
+
+      trap - EXIT INT TERM
+    }
+
+    upgrade_deps() {
+      local mode=''${1:-"minor"}
+
+      case $mode in
+        "major")
+          echo "Upgrading to latest major versions..."
+          yarn upgrade --latest 2>/dev/null
+          ;;
+        "minor")
+          echo "Upgrading to latest minor versions..."
+          yarn upgrade 2>/dev/null
+          ;;
+        "patch")
+          echo "Upgrading patch versions only..."
+          yarn upgrade --pattern "*" --target patch 2>/dev/null
+          ;;
+        *)
+          echo "Unknown mode: $mode"
+          echo "Usage: upgrade_deps [major|minor|patch]"
+          return 1
+          ;;
+      esac
+
+      yarn install 2>/dev/null
+
+      echo "Done! Don't forget to commit the updated yarn.lock file."
+      exit 0
+    }
+
+    export -f upgrade_deps
+    export -f serve
+
+    cleanup() {
+      echo "Cleaning up..."
+      rm -rf node_modules _site package.json
+    }
+
+    trap cleanup EXIT
+
+    echo "Development environment ready!"
+    echo "Run 'serve' to start development server"
+    echo "Run 'upgrade_deps [major|minor|patch]' to upgrade your dependencies"
+  '';
+}
